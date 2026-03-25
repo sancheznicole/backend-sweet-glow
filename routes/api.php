752c9339a8675b950fae5e2c76d13e880f465298
Mariana Sanchez <sanchezesquivel.mariana.82@gmail.com>
2026-03-24 23:58:11 -0500

    fix: resolver conflicto de merge en api.php

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ImagenesController;
 
@@ -18,19 +17,21 @@ Route::group([
 
 Route::apiResource('roles', App\Http\Controllers\RolesController::class);
 Route::apiResource('users', App\Http\Controllers\UsuariosController::class);
+Route::get('users/carts/{id}', [App\Http\Controllers\UsuariosController::class, 'findCarts']);
+
 Route::apiResource('categories', App\Http\Controllers\CategoriasController::class);
 Route::apiResource('brands', App\Http\Controllers\MarcasController::class);
 Route::apiResource('product_references', App\Http\Controllers\ReferenciaProductosController::class);
 Route::apiResource('gift_guide', App\Http\Controllers\GuiaRegalosController::class);
-
 Route::apiResource('products', App\Http\Controllers\ProductosController::class);
-
 Route::apiResource('images', App\Http\Controllers\ImagenesController::class);
 Route::apiResource('premios', App\Http\Controllers\PremioController::class);
 Route::apiResource('premiados', App\Http\Controllers\PremiadosController::class);
 Route::apiResource('gift_cards', App\Http\Controllers\TarjetasRegaloController::class);
 Route::apiResource('order_invoice', App\Http\Controllers\FacturaPedidosController::class);
+Route::apiResource('invoices_carts', App\Http\Controllers\FacturasCarritosController::class);
 Route::apiResource('carts', App\Http\Controllers\CarritosController::class);
+Route::apiResource('cart-elements', App\Http\Controllers\ElementosCarritosController::class);
 
 // Ruta personalizada ANTES del resource para evitar conflictos
 Route::get('gift_registrations/invoices-by-user/{id_usuario}', [App\Http\Controllers\InscripcionesRegaloController::class, 'facturasPorUsuario']);
@@ -38,3 +39,6 @@ Route::apiResource('gift_registrations', App\Http\Controllers\InscripcionesRegal
 
 Route::apiResource('reviews', App\Http\Controllers\ResenasController::class);
 Route::get('reviews/product/{id}', [App\Http\Controllers\ResenasController::class, 'getByProduct']);
+
+Route::post('/create-preference', [App\Http\Controllers\PaymentController::class, 'createPreference']);
+Route::post('/webhook/mercadopago', [App\Http\Controllers\WebhookController::class, 'handle']);
